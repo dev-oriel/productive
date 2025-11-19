8e05f2f6be51471defa51f34d841401832ee69e3
joe052 <joekaranjasenior52@gmail.com>
2025-11-19 13:25:23 +0300

    resolved task conflicts

--- productive-app/src/app/(main)/tasks/page.tsx
+++ productive-app/src/app/(main)/tasks/page.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import NewTask from "@/components/task/NewTask";
 import { taskApi } from "@/lib/services/api";
 import TaskList from "@/components/task/TaskList";
+import TaskLander from "@/components/task/TaskLander";
+import TaskCard from "@/components/task/TaskCard";
 
 /**COMPONENT */
 const Tasks: React.FC = () => {
@@ -12,8 +14,10 @@ const Tasks: React.FC = () => {
   /**TEMPLATE */
   return (
     <div className="">
+      {/* <TaskLander/> */}
+      {/* <NewTask/> */}
       <TaskList/>
-      {/* <TaskCard /> */}
+      <TaskCard />
     </div>
   );
 };
