b57ab6b9bd15a80799996fa4512c1df1101ab5d3
Carol <njorogecarol174@gmail.com>
2025-11-21 11:18:47 -0800

    Added formik and yup

--- productive-app/src/components/task/NewTask.tsx
+++ productive-app/src/components/task/NewTask.tsx
@@ -1,13 +1,37 @@
 "use client";
 
 import React from "react";
-import { Toaster, toast } from "sonner";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { toast } from "sonner";
 
-/**COMPONENT */
+/** COMPONENT */
 const NewTask: React.FC = () => {
   /**VARIABLES */
+  const TaskSchema = Yup.object().shape({
+    title: Yup.string().required("Task title is required"),
+    description: Yup.string(),
+    dueDate: Yup.date().nullable(),
+    priority: Yup.string().required(),
+  });
 
-  /**FUNCTIONS */
+  const initialValues = {
+    title: "",
+    description: "",
+    dueDate: "",
+    priority: "medium",
+  };
+
+  /**FUNCTIONS*/
+  const handleSubmit = (values: any, { resetForm }: any) => {
+    console.log("Task Created:", values);
+
+    toast("Task created successfully!", {
+      className: "bg-sky-300 text-white rounded-xl shadow-md px-4 py-2",
+    });
+
+    resetForm();
+  };
 
   /**TEMPLATE */
   return (
@@ -15,96 +39,107 @@ const NewTask: React.FC = () => {
       <div className="w-full max-w-xl rounded-xl border border-gray-200 shadow-sm p-8 bg-rgb(252, 252, 252)">
         <h2 className="text-lg font-semibold mb-6">Create New Task</h2>
 
-        <div className="space-y-6">
-          {/* Task Title */}
-          <div>
-            <label className="block text-sm font-medium mb-1">
-              Task Title *
-            </label>
-            <input
-              type="text"
-              placeholder="e.g., Finish project report"
-              className="w-full border border-gray-300 rounded-lg px-3 py-2 focus:ring-2 focus:ring-green-500 focus:outline-none"
-              required
-            />
-          </div>
-
-          {/* Description */}
-          <div>
-            <label className="block text-sm font-medium mb-1">
-              Description
-            </label>
-            <textarea
-              placeholder="Add any additional details..."
-              className="w-full border border-gray-300 rounded-lg px-3 py-2 focus:ring-2 focus:ring-green-500 focus:outline-none"
-            />
-          </div>
-
-          {/* Date & Time */}
-          <div>
-            <label className="block text-sm font-medium mb-1">Due Date</label>
-            <div className="flex gap-3">
-              <input
-                type="date"
-                className="w-1/2 border border-gray-300 rounded-lg px-3 py-2 focus:ring-2 focus:ring-green-500 focus:outline-none"
-              />
-              {/* <input
-                type="time"
-                className="w-1/2 border border-gray-300 rounded-lg px-3 py-2 focus:ring-2 focus:ring-green-500 focus:outline-none"
-              /> */}
-            </div>
-          </div>
-
-          {/* Priority */}
-          <div>
-            <label className="block text-sm font-medium mb-2">Priority</label>
-
-            <div className="flex items-center gap-6">
-              <label className="flex items-center gap-2 cursor-pointer">
-                <input type="radio" name="priority" value="low" />
-                <span>Low</span>
-              </label>
-
-              <label className="flex items-center gap-2 cursor-pointer">
-                <input
-                  type="radio"
-                  name="priority"
-                  value="medium"
-                  defaultChecked
+        <Formik
+          initialValues={initialValues}
+          validationSchema={TaskSchema}
+          onSubmit={handleSubmit}
+        >
+          {({ handleReset }) => (
+            <Form className="space-y-6">
+              {/**TASK TITLE */}
+              <div>
+                <label className="block text-sm font-medium mb-1">
+                  Task Title *
+                </label>
+                <Field
+                  name="title"
+                  type="text"
+                  placeholder="e.g., Finish project report"
+                  className="w-full border border-gray-300 rounded-lg px-3 py-2 
+                    focus:ring-2 focus:ring-green-500 focus:outline-none"
                 />
-                <span>Medium</span>
-              </label>
+                <ErrorMessage
+                  name="title"
+                  component="p"
+                  className="text-red-500 text-sm mt-1"
+                />
+              </div>
 
-              <label className="flex items-center gap-2 cursor-pointer">
-                <input type="radio" name="priority" value="high" />
-                <span>High</span>
-              </label>
-            </div>
-          </div>
+              {/* DESCRIPTION  */}
+              <div>
+                <label className="block text-sm font-medium mb-1">
+                  Description
+                </label>
+                <Field
+                  as="textarea"
+                  name="description"
+                  placeholder="Add any additional details..."
+                  className="w-full border border-gray-300 rounded-lg px-3 py-2 
+                    focus:ring-2 focus:ring-green-500 focus:outline-none"
+                />
+              </div>
 
-          {/* Buttons */}
-          <div className="flex justify-end gap-4 pt-4">
-            <button
-              type="button"
-              className="px-4 py-2 rounded-lg border border-gray-300 hover:bg-gray-100"
-            >
-              Cancel
-            </button>
+              {/**DUE DATE */}
+              <div>
+                <label className="block text-sm font-medium mb-1">
+                  Due Date
+                </label>
+                <div className="flex gap-3">
+                  <input
+                    type="date"
+                    className="w-full border border-gray-300 rounded-lg px-3 py-2 focus:ring-2 focus:ring-green-500 focus:outline-none"
+                  />
+                </div>
+              </div>
 
-            <button
-              onClick={() =>
-                toast("Task created successfully!", {
-                  className:
-                    "bg-sky-300 text-white rounded-xl shadow-md px-4 py-2",
-                })
-              }
-              type="button"
-              className="px-5 py-2 rounded-lg bg-green-600 text-white hover:bg-green-700"
-            >
-              Create Task
-            </button>
-          </div>
-        </div>
+              {/**PRIORITY */}
+              <div>
+                <label className="block text-sm font-medium mb-2">
+                  Priority
+                </label>
+                <div className="flex items-center gap-6">
+                  <label className="flex items-center gap-2 cursor-pointer">
+                    <Field type="radio" name="priority" value="low" />
+                    <span>Low</span>
+                  </label>
+
+                  <label className="flex items-center gap-2 cursor-pointer">
+                    <Field type="radio" name="priority" value="medium" />
+                    <span>Medium</span>
+                  </label>
+
+                  <label className="flex items-center gap-2 cursor-pointer">
+                    <Field type="radio" name="priority" value="high" />
+                    <span>High</span>
+                  </label>
+                </div>
+                <ErrorMessage
+                  name="priority"
+                  component="p"
+                  className="text-red-500 text-sm mt-1"
+                />
+              </div>
+
+              {/**BUTTONS */}
+              <div className="flex justify-end gap-4 pt-4">
+                <button
+                  type="button"
+                  onClick={handleReset}
+                  className="px-4 py-2 rounded-lg border border-gray-300 hover:bg-gray-100"
+                >
+                  Cancel
+                </button>
+
+                <button
+                  type="submit"
+                  className="px-5 py-2 rounded-lg bg-green-600 text-white hover:bg-green-700"
+                >
+                  Create Task
+                </button>
+              </div>
+            </Form>
+          )}
+        </Formik>
       </div>
     </div>
   );
