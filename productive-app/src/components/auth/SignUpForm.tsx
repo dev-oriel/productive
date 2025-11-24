4c8f58f2a8a13078420c4b9b7c2c70c5cfa901fe
dev-oriel <kiplangatoriel@gmail.com>
2025-11-24 12:47:55 +0300

    Modified the signup button

--- productive-app/src/components/auth/SignUpForm.tsx
+++ productive-app/src/components/auth/SignUpForm.tsx
@@ -268,7 +268,7 @@ const SignUpForm: React.FC = () => {
                   <button
                     type="submit"
                     disabled={!isValid || loading}
-                    className={`w-full py-2 px-4 border border-transparent rounded-full shadow-md text-sm font-medium text-white bg-green-500 hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 transition duration-150 disabled:bg-green-300 disabled:cursor-not-allowed disabled:blur-sm`}
+                    className={`w-full py-2 px-4 border border-transparent rounded-md shadow-md text-sm font-medium text-white bg-green-500 hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 transition duration-150 disabled:bg-green-300 disabled:cursor-not-allowed `}
                   >
                     {loading ? "Creating Account..." : "Create Account"}
                   </button>
